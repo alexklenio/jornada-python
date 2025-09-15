046ababf957970351624f864524b32b2027dd066
alexklenio <alexlopes.see@gmail.com>
2025-09-15 20:48:15 -0300

    Update codigo.py

--- Aula 1 - Python PowerUp/codigo.py
+++ Aula 1 - Python PowerUp/codigo.py
@@ -8,6 +8,39 @@ from time import sleep
 # pyautogui.click -> clicar em algum lugar da tela
 # pyautogui.hotkey -> combinação de teclas
 
+
+pyautogui.PAUSE = 0.5
+
+
+# abrir o navegador (chrome)
 pyautogui.press("win")
+pyautogui.write("chrome")  
+
+pyautogui.press("enter")
+sleep(2)
+pyautogui.click(x=765, y=377)
+sleep(2)
+
+# entrar no link 
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 sleep(1)
-pyautogui.write("chrome")
+pyautogui.press("enter")
+sleep(1)
+
+
+# Passo 2: Fazer login
+# selecionar o campo de email
+pyautogui.click(x=660, y=410, clicks=2)
+
+sleep(1)
+
+# escrever o seu email
+pyautogui.write("pythonimpressionador@gmail.com")
+sleep(1)
+pyautogui.press("tab")
+pyautogui.write("sua senha")
+pyautogui.press("tab")
+pyautogui.press("enter")
+sleep(3)
+pyautogui.click(x=850, y=362)
+
