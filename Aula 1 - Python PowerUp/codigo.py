729fe88fea8491b2e7252b48d3d276510234d059
alexklenio <alexlopes.see@gmail.com>
2025-09-15 21:52:56 -0300

    Update codigo.py

--- Aula 1 - Python PowerUp/codigo.py
+++ Aula 1 - Python PowerUp/codigo.py
@@ -12,13 +12,12 @@ import pandas as pd
 
 pyautogui.PAUSE = 0.5
 
-
 # abrir o navegador (chrome)
 pyautogui.press("win")
 pyautogui.write("chrome")  
 
 pyautogui.press("enter")
-sleep(2)
+sleep(3)
 pyautogui.click(x=765, y=377)
 sleep(2)
 
@@ -27,7 +26,6 @@ pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 sleep(1)
 pyautogui.press("enter")
 sleep(1)
-
 
 # Passo 2: Fazer login
 # selecionar o campo de email
@@ -44,7 +42,31 @@ pyautogui.press("tab")
 pyautogui.press("enter")
 sleep(3)
 pyautogui.click(x=850, y=362)
-
+sleep(1)
 
 tabela = pd.read_csv("produtos.csv")
+print(tabela)
 
+
+
+for linha in tabela.index:
+    pyautogui.click(x=640, y=294)
+
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter") # cadastra o produto (botao enviar)
+    pyautogui.scroll(5000)
